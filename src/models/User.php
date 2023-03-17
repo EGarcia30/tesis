de8ac3f1d9bd453dcd84ff7474fa02f77ec640d5
Erick Penado <penado188@gmail.com>
2023-03-17 13:26:26 -0600

    Ajuste de metodos principales en user

--- src/models/User.php
+++ src/models/User.php
@@ -3,40 +3,60 @@
 namespace Penad\Tesis\models;
 
 use Penad\Tesis\lib\Model;
+use Penad\Tesis\lib\Database;
 use PDO;
 use PDOException;
 
 class User extends Model{
 
     private int $_id;
-    private string $_usuario; 
-    private string $_clave;
-    private string $_rol_usuario;
-    private string $_nombre;
+    private string $_username; 
+    private string $_password;
+    private string $_user_role;
+    private string $_name;
 
-    public function __construct(string $usuario, string $clave, string $rol_usuario, string $nombre){
+    public function __construct(string $username, string $password, string $user_role, string $name){
         parent::__construct(); 
-        $this->_usuario = $usuario;
-        $this->_clave = $clave;
-        $this->_rol_usuario = $rol_usuario;
-        $this->_nombre = $nombre;
+        $this->_username = $username;
+        $this->_password = $password;
+        $this->_user_role = $user_role;
+        $this->_name = $name;
         // $this->db = $this->_db->connect();
+    }
+
+    public static function getUser(string $name):User{
+        try{
+            $_db = new Database();
+            $sql = "SELECT * FROM usuarios WHERE usuario=$name";
+            $query = $_db->connect()->query($sql);
+            $res = $query->fetch(PDO::FETCH_ASSOC);
+            
+            $user = new User($res['usuario'], $res['clave'], $res['rol_usuario'], $res['nombre']);
+            $user->setId($res['usuario_id']);
+            
+            return $user;
+            
+        }
+        catch(PDOException $e){
+            error_log($e->getMessage());
+            return NULL;
+        }
     }
 
     //insertar usuarios
     public function insertUser(){
         try{
             //TODO: validar si existe usuario
-            $data = $this->getExists($this->_usuario);
-            $validation = intval($data[0]['num']);
-            if($validation === 0){
-                $hash = $this->getHashedPassword($this->_clave);
+            $data = $this->getExists($this->_username);
+            $validation = intval($data['num']);
+            if(empty($data['num'])){
+                $hash = $this->getHashedPassword($this->_password);
                 $sql = 'INSERT INTO usuarios (usuario, clave, rol_usuario, nombre) VALUES(:usuario, :clave, :rol_usuario, :nombre)';
                 $query = $this->prepare($sql);
-                $query->bindValue(':usuario',$this->_usuario);
+                $query->bindValue(':usuario',$this->_username);
                 $query->bindValue(':clave',$hash);
-                $query->bindValue(':rol_usuario',$this->_rol_usuario);
-                $query->bindValue(':nombre',$this->_nombre);
+                $query->bindValue(':rol_usuario',$this->_user_role);
+                $query->bindValue(':nombre',$this->_name);
                 $res = $query->execute();
                 return $res;
             }
@@ -53,11 +73,12 @@ class User extends Model{
 
 
     //obtener usuario
-    public function getExists(string $user){
+    public static function getExists(string $user){
         try{
+            $_db = new Database();
             $sql = "SELECT COUNT(*) as num FROM usuarios WHERE usuario=$user";
-            $query = $this->query($sql);
-            $res = $query->fetchall(PDO::FETCH_ASSOC);
+            $query = $_db->connect()->query($sql);
+            $res = $query->fetch(PDO::FETCH_ASSOC);
             return $res;
 
         }
@@ -68,8 +89,13 @@ class User extends Model{
     }
 
     //encriptar contraseñas
-    private function getHashedPassword($clave){
-        return password_hash($clave, PASSWORD_DEFAULT, ['cost' == 10]);
+    private function getHashedPassword(string $password){
+        return password_hash($password, PASSWORD_DEFAULT, ['cost' == 10]);
+    }
+
+    //comprar contraseña
+    public function comparePassword(string $password):bool{
+        return password_verify($password, $this->_password);
     }
 
     public function getId(){
@@ -80,12 +106,12 @@ class User extends Model{
         return $this->_id = $value;
     }
 
-    public function getUser(){
-        return $this->_usuario;
+    public function getUsername(){
+        return $this->_username;
     }
 
-    public function setUser(string $value){
-        return $this->_usuario = $value;
+    public function setUsername(string $value){
+        return $this->_username = $value;
     }
 
     public function getRol(){
@@ -93,14 +119,14 @@ class User extends Model{
     }
 
     public function setRol(string $value){
-        return $this->_rol_usuario = $value;
+        return $this->_user_role = $value;
     }
 
     public function getName(){
-        return $this->_nombre;
+        return $this->_name;
     }
 
     public function setName(string $value){
-        return $this->_nombre = $value;
+        return $this->_name = $value;
     }
 }
